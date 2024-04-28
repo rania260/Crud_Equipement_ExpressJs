0d949f468495b822ea543b3cfdb68f5dc67dd6f1
rania260 <raniarezgui4@gmail.com>
2024-04-28 13:22:21 +0100

    change

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 //configuration mte3 lbase de donnee
 module.exports = {
     //mongo cloud
-    url:"mongodb+srv://maaouika07:maitenancepwd@cluster0.sgz7msq.mongodb.net/maintenanceapp?retryWrites=true&w=majority"
+    url:""
 }
